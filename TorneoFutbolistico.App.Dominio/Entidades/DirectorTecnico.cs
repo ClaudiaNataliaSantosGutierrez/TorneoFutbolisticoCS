36d2a962b3be5e90d7f72a8f07e7a295566ba265
Darly Estefania JImenez Acosta <90589973+Estefa0618@users.noreply.github.com>
2021-10-08 00:48:42 -0500

    Validacion Director

--- TorneoFutbolistico.App.Dominio/Entidades/DirectorTecnico.cs
+++ TorneoFutbolistico.App.Dominio/Entidades/DirectorTecnico.cs
@@ -3,6 +3,8 @@ namespace TorneoFutbolistico.App.Dominio
     public class DirectorTecnico : Persona
     {
         public int Id_DS { get; set; }
+        [Required(ErrorMessage = "El Equipo es obligatorio")]
+        [Display(Name = "Equipo al que pertenece")]
         public Equipo Equipo { get; set; }
     }
 }
