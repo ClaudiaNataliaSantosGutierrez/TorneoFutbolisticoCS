c49440954df6a6f584b65f73ad86655f4099dc21
Darly Estefania JImenez Acosta <90589973+Estefa0618@users.noreply.github.com>
2021-10-08 00:50:46 -0500

    Validación Municipio

--- TorneoFutbolistico.App.Dominio/Entidades/Municipio.cs
+++ TorneoFutbolistico.App.Dominio/Entidades/Municipio.cs
@@ -6,6 +6,7 @@ namespace TorneoFutbolistico.App.Dominio
     public class Municipio
     {
         public int Id { get; set; } 
+        [Required(ErrorMessage = "El Nombre de Municipio es obligatorio")]
         public string NombreMunicipio { get; set; }
         public Equipo Equipo { get; set; }
         ///public Estadio Estadio { get; set; }
