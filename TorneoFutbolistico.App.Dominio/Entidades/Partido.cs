accad68204076f72dfad0b1850a0ba9989787b81
Darly Estefania JImenez Acosta <90589973+Estefa0618@users.noreply.github.com>
2021-10-08 00:51:48 -0500

    Validación Partido

--- TorneoFutbolistico.App.Dominio/Entidades/Partido.cs
+++ TorneoFutbolistico.App.Dominio/Entidades/Partido.cs
@@ -7,9 +7,12 @@ namespace TorneoFutbolistico.App.Dominio
     public class Partido
     {
         public int Id { get; set; } 
+        [Required(ErrorMessage = "La Fecha/Hora es obligatoria")]
         public string FechaHora  { get; set; }
+        [Required(ErrorMessage = "El Equipo local es obligatorio")]
         public string EquipoLocal { get; set; }
         public int MarcadorLocal { get; set; }
+        [Required(ErrorMessage = "El equipo Visitante es obligatorio")]
         public string EquipoVisitante { get; set; }
         public int MarcadorVisitante { get; set; }
         public Estadio Estadio { get; set; }
