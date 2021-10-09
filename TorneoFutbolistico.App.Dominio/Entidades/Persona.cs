9fec719a25afa2c1f98a8a8521b3b01f7bd586da
Claudia Natalia Santos Gutiérrez <claudiasantosgutierrez38@gmail.com>
2021-10-09 17:55:19 -0500

    Actualizacion Entidad Arbitros

--- TorneoFutbolistico.App.Dominio/Entidades/Persona.cs
+++ TorneoFutbolistico.App.Dominio/Entidades/Persona.cs
@@ -1,14 +1,18 @@
 //using system;
-
+using System.ComponentModel.DataAnnotations;
 namespace TorneoFutbolistico.App.Dominio
 { 
     
     public class Persona
     {
         public int id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
         public string Nombre { get; set; }
+        [Required(ErrorMessage = "El apellido es obligatorio")]
         public string Apellidos { get; set; }
+        [Required(ErrorMessage = "El documento es obligatorio")]
         public int Documento { get; set; }
+        [Required(ErrorMessage = "El NumeroTelefono es obligatorio")]
         public string NumeroTelefono { get; set; }
 
     }
